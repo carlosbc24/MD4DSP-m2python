9ca87c504e96297fad928b1c3e43b914cbfb1730
CCamberoR <ccr@unex.es>
2024-02-08 13:01:20 +0100

    Cambios menores en doc interna.

--- functions/contract_invariants.py
+++ functions/contract_invariants.py
@@ -9,9 +9,21 @@ from helpers.enumerations import Belong, Operator, Closure, DataType, DerivedTyp
 
 class ContractsInvariants:
     """
-    This class is responsible for checking invariants of contracts.
+
     """
     def checkInv_FixValue_FixValue(self, dataDictionary: pd.DataFrame, dataTypeInput: DataType, FixValueInput, dataTypeOutput: DataType, FixValueOutput, axis_param: int=0) -> pd.DataFrame:
+        """
+        Check the invariant of the FixValue - FixValue relation
+        params:
+            dataDictionary: dataframe with the data
+            dataTypeInput: data type of the input value
+            FixValueInput: input value to check
+            dataTypeOutput: data type of the output value
+            FixValueOutput: output value to check
+            axis_param: axis to check the invariant
+        Returns:
+            dataDictionary with the FixValueInput and FixValueOutput values changed to the type dataTypeInput and dataTypeOutput respectively
+        """
         FixValueInput, FixValueOutput=cast_type_FixValue(dataTypeInput, FixValueInput, dataTypeOutput, FixValueOutput)
         #Función auxiliar que cambia los valores de FixValueInput y FixValueOutput al tipo de dato en DataTypeInput y DataTypeOutput respectivamente
         # Check FixValue - FixValue
