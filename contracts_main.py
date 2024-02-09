a8f0aae682d103c52606d770754d2388e58294d3
CarlosCambero <ccamberoo@alumnos.unex.es>
2024-02-09 20:36:29 +0100

    Adición de la función cast_type_FixValue y de checkInv_FixValue_Derived

--- contracts_main.py
+++ contracts_main.py
@@ -3,6 +3,9 @@
 from helpers.logger import set_logger
 from tests.contract_pre_post.simple_test import ContractSimpleTest
 from tests.contract_pre_post.tests_spotify_dataset import ContractExternalDatasetTests
+from functions.contract_invariants import ContractsInvariants
+import pandas as pd
+from helpers.enumerations import DataType, DerivedType
 
 if __name__ == "__main__":
 
@@ -10,10 +13,27 @@ if __name__ == "__main__":
     set_logger("test")
 
     # Execute all simple tests
-    contract_test = ContractSimpleTest()
-    contract_test.executeAll_SimpleTests()
+    # contract_test = ContractSimpleTest()
+    # contract_test.executeAll_SimpleTests()
+    #
+    # # Execute all external dataset tests
+    # ContractTestWithDatasets = ContractExternalDatasetTests()
+    # ContractTestWithDatasets.executeAll_ExternalDatasetTests()
 
-    # Execute all external dataset tests
-    ContractTestWithDatasets = ContractExternalDatasetTests()
-    ContractTestWithDatasets.executeAll_ExternalDatasetTests()
 
+
+    contracts = ContractsInvariants()
+
+    # datadic= pd.DataFrame({'A': [1, 2, 3, 4, 5], 'B': [1, 2, 3, 4, 5], 'C': [1, 2, 3, 4, 5]})
+    # result=contracts.checkInv_FixValue_FixValue(dataDictionary=datadic, dataTypeInput=DataType(2), FixValueInput=1,
+    #                                   dataTypeOutput=DataType(6), FixValueOutput=3)
+    #
+    # print(datadic)
+    # print(result)
+
+    datadic=pd.DataFrame({'A': [0, 2, 3, 5, 5], 'B': [1, 2, 3, 4, 5], 'C': [1, 2, 3, 4, 5]})
+    result=contracts.checkInv_FixValue_DerivedValue(dataDictionary=datadic, dataTypeInput=DataType(2), FixValueInput=0,
+                                      derivedTypeOutput=DerivedType(0), axis_param=0)
+
+    print(datadic)
+    print(result)
