4592f091be6faedb9f4b41d34b764c2fca051c2d
carlosbc24 <cbc@unex.es>
2025-06-24 13:16:51 +0200

    extended data smell function check_types_as_string to filter null nan or None values

--- functions/data_smells.py
+++ functions/data_smells.py
@@ -183,6 +183,8 @@ def check_types_as_string(data_dictionary: pd.DataFrame, field: str, expected_ty
 
     # Convert values to string, remove NaN and strip whitespace
     values = data_dictionary[field].replace('nan', np.nan).dropna().astype(str).str.strip()
+    # Filter out common invalid values
+    values = values[~values.isin(['nan', 'NA', 'NaN', 'null', 'NULL', 'None', '', null, None])]
     col_dtype = data_dictionary[field].dtype
 
     # If the expected type is String, check if all values are actually another type (integer, float, time, date, datetime)
